086ca0724c5095b704f6c4eb6f15ea30e0e30a6b
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-30 12:12:35 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuMakanan from './Page/MenuMakanan';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <MenuMakanan />
+    </div>
+  );
+}
+
+export default App;
